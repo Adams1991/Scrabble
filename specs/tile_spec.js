b4e0f0f323b4e16162b8b092840049d19aef33db
Adams1991 <adams121@hotmail.co.uk>
2018-08-17 11:29:01 +0100

    can create tile and tested can get letter value

--- specs/tile_spec.js
+++ specs/tile_spec.js
@@ -1,2 +1,17 @@
 const assert = require('assert');
 const Tile = require('../client/src/models/tile.js');
+
+describe("Tile", () => {
+
+  let tile;
+
+  beforeEach(() => {
+    tile = new Tile("A", 1);
+  })
+
+  it("should be able to get letter", () => {
+    const result = tile.letter;
+    assert.strictEqual(result, "A");
+  })
+
+});
