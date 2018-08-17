5fea28aa154251aa502729d28e90dccff2ecfe27
StuartCharters <charters.stuart@gmail.com>
2018-08-17 11:32:24 +0100

    can add a tile to a square and get tile back

--- specs/square_spec.js
+++ specs/square_spec.js
@@ -1,17 +1,26 @@
 const assert = require('assert');
 const Square = require('../client/src/models/square.js');
+const Tile = require('../client/src/models/tile.js');
 
 describe("Square", () => {
 
   let square;
+  let tile;
 
   beforeEach(() => {
+    tile = new Tile(`A`, 1);
     square = new Square();
   })
 
   it("should start without a tile", () => {
     const result = square.tile;
     assert.strictEqual(result, null);
-  })
+  });
+
+  it("should be able to set square to have a tile", () => {
+    square.tile = tile;
+    const result = square.tile;
+    assert.deepStrictEqual(result, tile);
+  });
 
 });
