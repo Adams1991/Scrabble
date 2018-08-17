712e233eeaefc8ce06b0b0231ce07c7c68f2c34c
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-08-17 14:00:23 +0100

    refactors bag to fill with tiles on creation

--- specs/bag_spec.js
+++ specs/bag_spec.js
@@ -16,61 +16,33 @@ describe("Bag", () => {
     bag = new Bag();
   });
 
-  it("should start with no tiles", () => {
-    const result = bag.tiles.length;
-    assert.strictEqual(result, 0);
-  });
-
   it("should be able to receive tile", () => {
+    bag.removeRandomTiles(100);
     bag.addTile(tile1);
     const result = bag.tiles;
     assert.deepStrictEqual(result, [tile1]);
   });
 
   it("should be able to receive tiles", () => {
+    bag.removeRandomTiles(100);
     bag.addTile(tile1);
     bag.addTiles([tile2, tile3]);
     const result = bag.tiles;
     assert.deepStrictEqual(result, [tile1, tile2, tile3]);
   });
 
-  it("should be able to receive new tile", () => {
-    bag.addNewTile("E", 1);
-    const result = bag.tiles;
-    const tile = new Tile("E", 1);
-    assert.deepStrictEqual(bag.tiles.length, 1);
-    assert.deepStrictEqual(bag.tiles[0].value, 1);
-    assert.deepStrictEqual(bag.tiles[0].letter, "E");
-    assert.deepStrictEqual(result, [tile]);
-  });
-
-  it("should be able to receive new tiles", () => {
-    bag.addNewTiles("F", 4, 2);
-    const result = bag.tiles;
-    const tile1 = new Tile("F", 4);
-    const tile2 = new Tile("F", 4);
-    const tiles = [tile1, tile2];
-    assert.deepStrictEqual(bag.tiles.length, 2);
-    assert.deepStrictEqual(bag.tiles[0].value, 4);
-    assert.deepStrictEqual(bag.tiles[1].letter, "F");
-    assert.deepStrictEqual(result, tiles);
-  });
-
-  it("can be filled", () => {
-    bag.fill();
+  it("should be full on creation", () => {
     assert.deepStrictEqual(bag.tiles.length, 100);
     assert.deepStrictEqual(bag.tiles[98].value, 0);
     assert.deepStrictEqual(bag.tiles[50].letter, "L");
   });
 
-  it("can remove random tile", () => {
-    bag.fill();
+  it("should be able to remove random tile", () => {
     const tile = bag.removeRandomTile()[0];
     assert.deepStrictEqual(bag.tiles.length, 99);
   });
 
-  it("can remove random tiles", () => {
-    bag.fill();
+  it("should be able to remove random tiles", () => {
     const tiles = bag.removeRandomTiles(7);
     assert.deepStrictEqual(bag.tiles.length, 93);
     assert.deepStrictEqual(tiles.length, 7);
