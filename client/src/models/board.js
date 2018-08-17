6cdcc42fad3e97cc5c0f1ae0da9b748d862d8877
StuartCharters <charters.stuart@gmail.com>
2018-08-17 14:34:55 +0100

    can create a squares for a board

---
+++ client/src/models/board.js
@@ -0,0 +1,55 @@
+const Square = require('./square.js');
+
+class Board {
+  constructor() {
+    this.squares = createSquares();
+  }
+}
+
+module.exports = Board;
+
+function createSquares() {
+  const startRowArray = [];
+  const mirrorLineArray = createMirrorLine();
+
+  for (var i = 0; i < 7; i++) {
+    const startColumnArray = [];
+
+    for (var j = 0; j < 7; j++) {
+      let square;
+      if (i > j) {
+        square = startRowArray[j][i];
+      }else{
+        // Logic for multipliers here
+        square = new Square();
+      };
+      startColumnArray.push(square);
+    };
+
+    const correctColumnArray = startColumnArray.map(square => square);
+    startColumnArray.reverse()
+    correctColumnArray.push(mirrorLineArray[i]);
+    console.log(startColumnArray.length);
+    const columnArray = correctColumnArray.concat(startColumnArray);
+    // console.log(columnArray.length);
+
+    startRowArray.push(columnArray);
+  };
+
+  const correctRowArray = startRowArray.map(row => row);
+  startRowArray.reverse();
+  correctRowArray.push(mirrorLineArray);
+  const rowArray = correctRowArray.concat(startRowArray);
+
+  return rowArray;
+};
+
+function createMirrorLine() {
+  const mirrorLineArray = [];
+  for (var i = 0; i < 15; i++) {
+    const square = new Square();
+    mirrorLineArray.push(square);
+  }
+  console.log(mirrorLineArray.length);
+  return mirrorLineArray;
+}
---
+++ specs/board_spec.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const Tile = require('../client/src/models/tile.js');
+const Board = require('../client/src/models/board.js');
+
+describe("Board", () => {
+
+  let board;
+  let tile;
+
+  beforeEach(() => {
+
+    board = new Board();
+    tile = new Tile(`A`, 1);
+
+  });
+
+  it("should have 225 squares when made", () => {
+    const result1 = board.squares.length;
+    const result2 = board.squares[0].length;
+    assert.strictEqual(result1, 15);
+    assert.strictEqual(result2, 15);
+  })
+
+});
