32261fb52f52691ebbf6484c199e31201a2a0891
Adams1991 <adams121@hotmail.co.uk>
2018-08-17 11:56:25 +0100

    tests rack model starts with empty array

--- client/src/models/rack.js
+++ client/src/models/rack.js
@@ -1,6 +1,10 @@
 class Rack {
   constructor() {
+    this.tiles = []
   };
+
+
+
 
 }
 module.exports = Rack;
--- specs/rack_spec.js
+++ specs/rack_spec.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const Tile = require('../client/src/models/tile.js');
 const Rack = require('../client/src/models/rack.js');
 
-describe("rack", () => {
+describe("Rack", () => {
 
   let rack;
   let tile;
@@ -11,5 +11,15 @@ describe("rack", () => {
     tile = new Tile(`A`, 1);
     rack = new Rack();
   })
+
+  it("should start without any tiles", () => {
+    const result = rack.tiles;
+    assert.deepStrictEqual(result, []);
+  });
+
+
+
+
+
 
 });
