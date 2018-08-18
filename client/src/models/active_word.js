01c3d342cfc07d22668e5a3dc679ad6e78619a14
Adams1991 <adams121@hotmail.co.uk>
2018-08-18 12:00:25 +0100

    adds tile in active word in correct direction

--- client/src/models/active_word.js
+++ client/src/models/active_word.js
@@ -1,8 +1,18 @@
 class ActiveWord {
   constructor(firstLocatedTile, secondLocatedTile) {
     this.tiles = [firstLocatedTile.tile, secondLocatedTile.tile];
-    console.log(firstLocatedTile.coord);
     this.direction = calculateDirection(firstLocatedTile.coord, secondLocatedTile.coord);
+  }
+
+  addTile(tilePlaced){
+    const direction = Object.keys(this.direction)[0];
+    const tileCoordToCheck = tilePlaced.coord[direction]
+    if(tileCoordToCheck === this.direction[direction]){
+      this.tiles.push(tilePlaced);
+      return true;
+    } else {
+      return false;
+    }
   }
 
 
--- specs/active_word_spec.js
+++ specs/active_word_spec.js
@@ -6,14 +6,17 @@ describe("Active Word", () => {
 
   let tile1;
   let tile2;
+  let tile3;
   let firstLocatedTile;
   let activeWord;
 
   beforeEach(() => {
     tile1 = new Tile("A", 1);
     tile2 = new Tile("B", 3);
+    tile3 = new Tile("E", 1);
     firstLocatedTile = {tile: tile1, coord: {x:0, y:12}}
     secondLocatedTile = {tile: tile2, coord: {x:0, y:13}}
+    thirdLocatedTile = {tile: tile3, coord: {x:0, y:14}}
     activeWord = new ActiveWord(firstLocatedTile, secondLocatedTile);
   });
 
@@ -21,5 +24,11 @@ describe("Active Word", () => {
     const result = activeWord.direction;
     assert.deepStrictEqual(result, {x:0});
   });
+
+  it("should be able to have tile added that has correct direction", () => {
+    activeWord.addTile(thirdLocatedTile);
+    const result = activeWord.tiles.length;
+    assert.deepStrictEqual(result, 3);
+  })
 
 });
