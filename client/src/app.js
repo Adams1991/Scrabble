f266c8767026490daeae55085662dee6c03ea623
StuartCharters <charters.stuart@gmail.com>
2018-08-20 12:08:32 +0100

    can input player names and submit them

--- client/src/app.js
+++ client/src/app.js
@@ -3,31 +3,37 @@ const RackView = require('./views/rack_view.js');
 const TransitionView = require('./views/transition_view.js');
 const PlayerView = require('./views/player_view.js');
 const ScoreView = require('./views/score_view.js');
+const NewGameView = require('./views/new_game_view.js');
 
-const Player = require('../../client/src/models/player.js');
-const Game = require('../../client/src/models/game.js');
-
-const player1 = new Player('Connor');
-const player2 = new Player('Amaya');
-const player3 = new Player('Greg');
-const players = [player1, player2, player3];
-const game = new Game(players);
+// const Player = require('../../client/src/models/player.js');
+// const Game = require('../../client/src/models/game.js');
+//
+// const player1 = new Player('Connor');
+// const player2 = new Player('Amaya');
+// const player3 = new Player('Greg');
+// const players = [player1, player2, player3];
+// const game = new Game(players);
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  const playerContainer = document.querySelector(`#player-wrapper`)
-  const playerView = new PlayerView(playerContainer)
-  playerView.bindEvents();
-  const transitionView = new TransitionView(playerContainer)
-  transitionView.bindEvents();
+  const newGameButton = document.querySelector(`button#new-game`)
+  const body = document.querySelector('body');
+  const newGameView = new NewGameView(newGameButton, body);
+  newGameView.bindEvents();
 
-  const scoreContainer = document.querySelector(`#score-wrapper`)
-  const scoreView = new ScoreView(scoreContainer);
-  scoreView.bindEvents();
-
-  const boardTable = document.querySelector('#board');
-  const boardView = new BoardView(boardTable);
-  boardView.bindEvents();
+  // const playerContainer = document.querySelector(`#player-wrapper`)
+  // const playerView = new PlayerView(playerContainer)
+  // playerView.bindEvents();
+  // const transitionView = new TransitionView(playerContainer)
+  // transitionView.bindEvents();
+  //
+  // const scoreContainer = document.querySelector(`#score-wrapper`)
+  // const scoreView = new ScoreView(scoreContainer);
+  // scoreView.bindEvents();
+  //
+  // const boardTable = document.querySelector('#board');
+  // const boardView = new BoardView(boardTable);
+  // boardView.bindEvents();
 
 });
 
--- client/src/views/new_game_view.js
+++ client/src/views/new_game_view.js
@@ -1,3 +1,51 @@
 
 
-//TODO: add player form
+class NewGameView {
+  constructor(button, container) {
+    this.container = container;
+    this.button = button;
+  }
+
+  bindEvents() {
+    this.button.addEventListener(`click`, () => {
+      this.container.innerHTML = '';
+      const form = document.createElement(`form`);
+
+      const listTitle = document.createElement(`h3`);
+      listTitle.textContent = `Players:`
+      form.appendChild(listTitle);
+
+      const playerList = document.createElement(`ol`);
+      playerList.id = `player-list`;
+      for (var i = 0; i < 2; i++) {
+        const player = document.createElement('li');
+        const playerInput = document.createElement(`input`);
+        playerInput.type = `text`;
+        playerInput.required = `true`;
+        playerInput.classList.add(`player-input`);
+        playerInput.name = `playerInput`;
+        player.appendChild(playerInput);
+        playerList.appendChild(player);
+      };
+      form.appendChild(playerList);
+
+      const submitButton = document.createElement(`input`);
+      submitButton.type = `submit`;
+      submitButton.value = `Start Game`;
+      form.appendChild(submitButton);
+      this.container.appendChild(form);
+
+      form.addEventListener(`submit`, (evt) => {
+        evt.preventDefault();
+        const playerDetails = evt.target.playerInput;
+        const players = [];
+        playerDetails.forEach(player => players.push(player.value));
+        console.dir(players);
+      });
+    });
+
+  }
+
+}
+
+module.exports = NewGameView;
