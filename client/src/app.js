45406ddf240c620073bec12ca6e26deda8a7f506
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-08-20 22:41:05 +0100

    refactors, adding form_view.js

--- client/src/app.js
+++ client/src/app.js
@@ -1,18 +1,19 @@
+const FormView = require('./views/form_view.js');
 const NewGameView = require('./views/new_game_view.js');
 const Submission = require('./models/submission.js');
-
-
-
 
 document.addEventListener('DOMContentLoaded', () => {
 
   const newGameButton = document.querySelector(`button#new-game`)
   const body = document.querySelector('body');
-  const newGameView = new NewGameView(newGameButton, body);
-  newGameView.bindEvents();
+  const formView = new FormView(newGameButton, body);
+  formView.bindEvents();
 
   const url = `http://localhost:3000/api/games`
   const submission = new Submission(url);
   submission.bindEvents();
+
+  const newGameView = new NewGameView(body);
+  newGameView.bindEvents();
 
 });
---
+++ client/src/views/form_view.js
@@ -0,0 +1,61 @@
+const PubSub = require('../helpers/pub_sub.js');
+const Player = require('../models/player.js');
+const Game = require('../models/game.js');
+// const Turn = require('../models/turn.js');
+// const BoardView = require('./board_view.js');
+// const TransitionView = require('./transition_view.js');
+// const PlayerView = require('./player_view.js');
+// const ScoreView = require('./score_view.js');
+
+class FormView {
+  constructor(button, container) {
+    this.container = container;
+    this.button = button;
+  };
+
+  bindEvents() {
+    this.button.addEventListener(`click`, () => {
+      this.container.innerHTML = '';
+      const form = document.createElement(`form`);
+
+      const listTitle = document.createElement(`h3`);
+      listTitle.textContent = `Players:`
+      form.appendChild(listTitle);
+
+      const playerList = document.createElement(`ol`);
+      playerList.id = `player-list`;
+      for (var i = 0; i < 2; i++) {
+        const player = document.createElement('li');
+        const playerInput = document.createElement(`input`);
+        playerInput.type = `text`;
+        playerInput.required = `true`;
+        playerInput.value = `Shaun`;
+        playerInput.classList.add(`player-input`);
+        playerInput.name = `playerInput`;
+        player.appendChild(playerInput);
+        playerList.appendChild(player);
+      };
+      form.appendChild(playerList);
+
+      const submitButton = document.createElement(`input`);
+      submitButton.type = `submit`;
+      submitButton.value = `Start Game`;
+      form.appendChild(submitButton);
+      this.container.appendChild(form);
+
+      form.addEventListener(`submit`, (evt) => {
+        evt.preventDefault();
+        const playerDetails = evt.target.playerInput;
+        const players = [];
+        playerDetails.forEach(playerName => {
+          const player = new Player(playerName.value);
+          players.push(player);
+        });
+        const game = new Game(players);
+        PubSub.publish(`Submission:new-game`, game);
+      });
+    });
+  };
+};
+
+module.exports = FormView;
